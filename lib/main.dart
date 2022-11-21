a46198ae99833baceb9dafbd28399a4114548160
L200180194 <alfiantoandy6@gmail.com>
2022-11-21 14:03:09 +0700

    slideshow

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'App Mitra Kedungdoro',
+      title: 'Layar Panggilan Tv',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           fontFamily: 'Open Sans',
--- lib/screen/video_screen.dart
+++ lib/screen/video_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_image_slider/carousel.dart';
+import 'package:flutter_image_slider/indicator/Circle.dart';
 
 import '../app/constant.dart';
 
@@ -11,6 +14,24 @@ class Video extends StatelessWidget {
       color: Colors.amberAccent,
       height: Constant.getActualY(context: context, Y: 1553),
       width: Constant.getActualX(context: context, x: 779),
+      child: Carousel(
+        height: Constant.getActualY(context: context, Y: 1553),
+        width: Constant.getActualX(context: context, x: 779),
+        indicatorBarColor: Colors.transparent,
+        autoScroll: true,
+        autoScrollDuration: Duration(seconds: 1),
+        stopAtEnd: false,
+        animationPageDuration: Duration(milliseconds: 600),
+        unActivatedIndicatorColor: Colors.transparent,
+        activateIndicatorColor: Colors.transparent,
+        items: [
+          Image.asset(
+            'images/slide_pramita_0.jpg',
+            fit: BoxFit.fill,
+          ),
+          Image.asset('images/slide_pramita_1.jpg', fit: BoxFit.fill)
+        ],
+      ),
     );
   }
 }
